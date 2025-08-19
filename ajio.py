efb39c67d707002fa7182251ec55224ac0ecb7b4
Vikas Rana <vikas.developercwc@gmail.comgit  config --global user.email vikas.developercwc@gmail.com>
2025-08-19 22:46:56 +0530

    first step autmated way

---
+++ ajio.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+s = Service(r"C:\Users\acer\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+
+driver.get("https://www.ajio.com/men-backpacks/c/830201001")
+time.sleep(5)
+
+# Get window height and scroll height
+height = driver.execute_script("return window.innerHeight")
+old_height = driver.execute_script("return document.body.scrollHeight")
+
+print("Window Height:", height)
+print("Total Scroll Height:", old_height)
+
+counter=1
+while True:
+# Scroll till bottom
+   driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # last page scroll down but problem it can be stop
+   time.sleep(3)
+
+   new_height=driver.execute_script("return document.body.scrollHeight")  # for last tak scroll
+
+   print(counter)
+   counter=+1
+   print(old_height)
+   print(new_height)
+
+   if new_height == old_height:   # all page  loaded the data
+       break
+   old_height=new_height
+
+html=driver.page_source
+
+####3--this code is save/load the html file
+with open("ajio.html","w",encoding="utf-8") as f:
+    f.write(html)
+
+print(height)
+
+
---
+++ app.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+chrome_options = Options()
+prefs = {"profile.default_content_setting_values.notifications": 2}  # 2 = Block
+chrome_options.add_experimental_option("prefs", prefs)
+
+s = Service(r"C:\Users\acer\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+driver.get("https://learnwith.campusx.in/")
+time.sleep(50)
+
+driver.get("https://learnwith.campusx.in/courses/Data-Science-Master-Program-63a30827e4b0c0f22f756324")
+time.sleep(5)
+
+link = driver.find_element(By.XPATH, '/html/body/div[1]/header/section[2]/a[8]')
+link.click()
+
+time.sleep(10)
+driver.quit()   #  this is the automated  code only  you can use  for automation
+
+
+
